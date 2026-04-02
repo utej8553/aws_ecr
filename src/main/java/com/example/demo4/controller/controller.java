1914dec9e563a84a909b6f1c1d16ed2c01b9b91b
Utej <91786744+utej8553@users.noreply.github.com>
2026-04-02 23:40:28 +0530

    added these

---
+++ src/main/java/com/example/demo4/controller/controller.java
@@ -0,0 +1,21 @@
+package com.example.demo4.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api")
+public class controller {
+    @GetMapping("/home")
+    String func1(){
+        return "You have just visited /home";
+    }
+
+    @GetMapping("/end")
+    String func2(){
+        return "You have just visited /end";
+    }
+}
