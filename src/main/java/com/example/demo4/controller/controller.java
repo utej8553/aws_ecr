87c7611078b5dbe0d087519276f001ecf4f1a9da
Utej <91786744+utej8553@users.noreply.github.com>
2026-04-17 14:26:22 +0530

    Update controller.java

--- src/main/java/com/example/demo4/controller/controller.java
+++ src/main/java/com/example/demo4/controller/controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
     @GetMapping("/home")
     String func1(){
-        return "You have just visited /home";
+        return "You have just visited /home6767";
     }
 
     @GetMapping("/end")
